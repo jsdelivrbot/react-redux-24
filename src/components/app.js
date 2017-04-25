49e2ce18872c9a28a9c963a41acb63f5ff3c0c56
Joe Saad <yousseffhs@yahoo.com>
2017-04-25 17:03:09 -0500

    Adding Scss loaders
    - Added sass-loader, css-loader and style-loader,
    - Added configuration to webpack.config.js
    - tested it by adding scss file and check if sass files are working..

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import './app.scss';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>React simple starter 123 545</div>
       );
   }
 }
