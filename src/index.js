db7ba81fe3e8c73a034237d0b0d6137606927bc2
Joe Saad <yousseffhs@yahoo.com>
2017-05-02 17:12:52 -0500

    Introducing Routers
    -Adding routers with dummy components for clarity of example.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+class Hello extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
+
+class World extends Component {
+  render() {
+    return (
+      <div>World</div>
+    )
+  }
+}
+
+render(
+  <Provider store={ createStoreWithMiddleware(reducers) }>
+    <Router history={ browserHistory }>
+      <Route path="/hello" component={ Hello } />
+      <Route path="/world" component={ World } />
+    </Router>
   </Provider>
   , document.querySelector('.container'));
